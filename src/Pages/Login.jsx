5d8c03509161b056c5367de2fe813e7fd27e5847
DevMeley <ayantolaAbisola@outlook.com>
2025-01-02 22:25:07 +0100

    something

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,57 +1,69 @@
-import React from 'react'
-import { useState } from 'react'
-import "../Pages CSS/Register.css"
-import { Link, useNavigate } from 'react-router-dom'
+import React from "react";
+import { useState } from "react";
+import "../Pages CSS/Register.css";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Login() {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const navigate = useNavigate()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
   const handleLoginSubmit = async (e) => {
-    e.preventDefault()
-    console.log(email, password)
+    e.preventDefault();
+    console.log(email, password);
 
     try {
-      const res = await fetch('/api/user/auth/login', {
+      const res = await fetch("/api/user/auth/login", {
         method: "POST",
-        body: JSON.stringify({email, password}),
+        body: JSON.stringify({ email, password }),
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
-      })
+      });
       const data = await res.json();
-      console.log(data)
+      console.log(data);
 
       // pick the token
-      const token = data.token
+      const token = data.token;
 
       // store the token in local storage
-      localStorage.setItem('jwtToken', token);
+      localStorage.setItem("jwtToken", token);
 
       // navigate
-      navigate("/")
+      navigate("/");
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
   return (
-        <div className="background">
-            <div className="signup-container">
-              <h2>Log in</h2>
-              <form onSubmit={handleLoginSubmit}>
-                <label>
-                  <p>Email</p>
-                  <input type="text" value={email} onChange={(e)=> setEmail(e.target.value)}/>
-                </label>
-                <label>
-                  <p>Password</p>
-                  <input type="text" value={password} onChange={(e)=> setPassword(e.target.value)}/>
-                </label>
-                <button type='submit' className='btn'>Log in</button>
-              </form>
-              <p>Don't have an account? <Link to="/auth/register">Sign up </Link></p>
-            </div>
-        </div>
-    )
+    <div className="background">
+      <div className="signup-container">
+        <h2>Log in</h2>
+        <form onSubmit={handleLoginSubmit}>
+          <label>
+            <p>Email</p>
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+          <label>
+            <p>Password</p>
+            <input
+              type="text"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+          <button type="submit" className="btn">
+            Log in
+          </button>
+        </form>
+        <p>
+          Don't have an account? <Link to="/auth/register">Sign up </Link>
+        </p>
+      </div>
+    </div>
+  );
 }
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,58 +1,73 @@
-import React, { useState } from 'react'
-import '../Pages CSS/Register.css'
-import { Link, useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
+import "../Pages CSS/Register.css";
+import { Link, useNavigate } from "react-router-dom";
 
-export default function() {
-  const [username, setUsername] = useState("")
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const navigate = useNavigate()
+export default function () {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-  const handleSubmit = async(e) =>{
-    e.preventDefault()
-    console.log(username, email, password)
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(username, email, password);
     try {
-      const res = await fetch('/api/user/auth/register', {
+      const res = await fetch("/api/user/auth/register", {
         method: "POST",
-        body: JSON.stringify({username, email, password}),
+        body: JSON.stringify({ username, email, password }),
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
-      })
+      });
       const data = await res.json();
-      console.log(data)
-      
-      // redirect to log in
-      navigate("/auth/login")
+      console.log(data);
 
+      // redirect to log in
+      navigate("/auth/login");
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   return (
     <div>
-        <div className="background">
-            <div className="signup-container">
-              <h2>Sign up</h2>
-              <form onSubmit={handleSubmit}>
-                <label>
-                  <p>Fullname</p>
-                  <input type="text" value={username} onChange={(e)=> setUsername(e.target.value)}/>
-                </label>
-                <label>
-                  <p>Email</p>
-                  <input type="text" value={email} onChange={(e)=> setEmail(e.target.value)}/>
-                </label>
-                <label>
-                  <p>Password</p>
-                  <input type="text" value={password} onChange={(e)=> setPassword(e.target.value)}/>
-                </label>
-                <button type='submit' className='btn'>Sign up</button>
-              </form>
-              <p>Already have an account? <Link to="/auth/login">Log in </Link></p>
-            </div>
+      <div className="background">
+        <div className="signup-container">
+          <h2>Sign up</h2>
+          <form onSubmit={handleSubmit}>
+            <label>
+              <p>Fullname</p>
+              <input
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </label>
+            <label>
+              <p>Email</p>
+              <input
+                type="text"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </label>
+            <label>
+              <p>Password</p>
+              <input
+                type="text"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </label>
+            <button type="submit" className="btn">
+              Sign up
+            </button>
+          </form>
+          <p>
+            Already have an account? <Link to="/auth/login">Log in </Link>
+          </p>
         </div>
+      </div>
     </div>
-  )
+  );
 }
